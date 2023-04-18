0e36f3f6b61916ccb7e3624c21ac05b5ceb11ce0
derFabio <fabio.virgolini@gmail.com>
2023-04-18 11:41:11 +0200

    FindWorkshopById done

--- backend/src/main/java/com/codecool/SellWorkshops/endpoint/WorkshopEndpoint.java
+++ backend/src/main/java/com/codecool/SellWorkshops/endpoint/WorkshopEndpoint.java
@@ -4,10 +4,12 @@ import com.codecool.SellWorkshops.entity.Workshop;
 import com.codecool.SellWorkshops.service.WorkshopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/workshops")
@@ -22,5 +24,10 @@ public class WorkshopEndpoint {
     @GetMapping(value = "/all")
     public List<Workshop> getAllWorkshops() {
         return workshopService.getAllWorkshops();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Optional<Workshop> getWorkshopById(@PathVariable Long id) {
+        return workshopService.getWorkshopById(id);
     }
 }
